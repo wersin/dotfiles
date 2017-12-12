d23f2d4064ec282d459925fae466c26f823c5b2a
mark <w3rsin@outlook.com>
2017-12-12 16:19:44 +0100

    update dots

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env sh
+# !/usr/bin/env sh
+#!/usr/biin/env zsh
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,7 +7,18 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+# doesnt work if not hard specified like below
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    # MONITOR=$m polybar --reload topbar &
+    echo $m
+  done
+else
+  polybar --reload topbar &
+fi
+
 # Launch bars
-polybar topbar
+MONITOR=VGA1 polybar topbar &
+MONITOR=LVDS1 polybar topbar &
 
 echo "Bars launched..."
